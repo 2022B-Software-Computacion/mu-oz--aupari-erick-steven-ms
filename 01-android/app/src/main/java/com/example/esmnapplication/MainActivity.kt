518463be5f1516210c26ff5a153cf74baed28f6c
Erick <erick.munoz@epn.edu.ec>
2022-12-22 09:09:39 -0500

    Ciclo de vida de Actividades

--- 01-android/app/src/main/java/com/example/esmnapplication/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/esmnapplication/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.esmnapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) //apunta a la carpeta de recursos
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida.setOnClickListener{
+            irActividad(ACicloVida::class.java)
+        }
+    }
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
