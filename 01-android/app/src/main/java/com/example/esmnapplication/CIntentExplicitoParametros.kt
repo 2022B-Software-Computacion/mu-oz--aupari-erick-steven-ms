ac15dbdc741fbfd6c1164ee803abc386c5233593
Erickneitor1416 <erick.munoz@epn.edu.ec>
2023-01-05 09:57:37 -0500

    init intents

---
+++ 01-android/app/src/main/java/com/example/esmnapplication/CIntentExplicitoParametros.kt
@@ -0,0 +1,11 @@
+package com.example.esmnapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CIntentExplicitoParametros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cintent_explicito_parametros)
+    }
+}
